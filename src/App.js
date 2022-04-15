ced8f0e6f144ec6654be38d1e17ffb647a59fc3d
nithkish <102673014+nithkish@users.noreply.github.com>
2022-04-16 01:10:21 +0530

    added main style

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React, { useState } from "react";
 import RouteDetails from "./RouteDetails";
 import stops from "./stops.json";
-
+import { Col, Row, Input } from "reactstrap";
+import "./styles.css";
 function App() {
   const [schedulingStrategy, setSchedulingStrategy] = useState("fixed");
   const [canAddCargo, setCanAddCargo] = useState(false);
 
   return (
     <div className="App">
-      <div>
-        <select
-          value={schedulingStrategy}
-          onChange={(e) => setSchedulingStrategy(e.target.value)}
-        >
-          <option value="fixed">Fixed</option>
-          <option value="semiFlexible">Semi flexible</option>
-          <option value="flexible">Flexible</option>
-        </select>
-        <label>
-          <input
-            type="checkbox"
-            checked={canAddCargo}
-            onChange={(e) => setCanAddCargo(e.target.checked)}
+      <Row>
+        <Col>
+          <Input
+            type="select"
+            className="drop-down"
+            value={schedulingStrategy}
+            onChange={(e) => setSchedulingStrategy(e.target.value)}
+          >
+            <option value="fixed">Fixed</option>
+            <option value="semiFlexible">Semi flexible</option>
+            <option value="flexible">Flexible</option>
+          </Input>
+        </Col>
+        <Col>
+          <label>
+            <input
+              type="checkbox"
+              checked={canAddCargo}
+              onChange={(e) => setCanAddCargo(e.target.checked)}
+            />
+            {" Can add cargo "}
+          </label>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <RouteDetails
+            stops={stops}
+            schedulingStrategy={schedulingStrategy}
+            canAddCargo={canAddCargo}
           />
-          Can add cargo
-        </label>
-      </div>
-      <RouteDetails
-        stops={stops}
-        schedulingStrategy={schedulingStrategy}
-        canAddCargo={canAddCargo}
-      />
+        </Col>
+      </Row>
     </div>
   );
 }
